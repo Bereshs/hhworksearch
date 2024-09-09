6a32c708438b3a5fe7b09cae8c182f8345d8bfc3
bereshs <bereshs@mail.ru>
2024-09-09 20:55:30 +0300

    templates favicon

--- src/main/java/ru/bereshs/hhworksearch/HhWorkSearchApplication.java
+++ src/main/java/ru/bereshs/hhworksearch/HhWorkSearchApplication.java
@@ -3,6 +3,7 @@ package ru.bereshs.hhworksearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableFeignClients
 @SpringBootApplication
